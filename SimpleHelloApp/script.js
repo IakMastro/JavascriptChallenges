3a5cc7996c4b726b87d9959b583a060b2339f97c
IakMastro <cse242017102@uniwa.gr>
2020-10-11 00:09:38 +0300

    First Challenge: A Simple Hello App

---
+++ SimpleHelloApp/script.js
@@ -0,0 +1,6 @@
+
+function submitButton() {
+    let name = document.getElementById("nameId").value;
+    let surname = document.getElementById("surnameId").value;
+    alert("Hello " + name + " " + surname + ", how are you?");
+}
